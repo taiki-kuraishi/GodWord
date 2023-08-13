2315b198bce66b04716bd98846f80bb75e6434c9
taiki-kuraishi <20122027co@gmail.com>
2023-08-13 20:37:29 +0900

    setup

---
+++ test_socketio/front/src/App.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="app">
+    <h5>test socketio</h5>
+    <!-- 入室済の場合、部屋の情報を表示 -->
+    <div v-if="isJoined">
+      <div>{{ userName }} さん</div>
+      部屋番号: {{ roomId }}
+    </div>
+
+    <!-- 未入室の場合、部屋を作る or 部屋に入るを選択 -->
+    <div v-else>
+      <div>名前: <input v-model="userName" type="text" /></div>
+
+      <input type="radio" v-model="joinType" value="1" />新しく部屋を作る
+      <input type="radio" v-model="joinType" value="2" />友達の部屋に入る
+
+      <div v-if="joinType == 1">
+        <input type="button" value="部屋を作る" @click="createRoom" />
+      </div>
+
+      <div v-if="joinType == 2">
+        部屋番号: <input v-model="roomId" type="text" />
+        <input type="button" value="入室" @click="enterRoom" />
+      </div>
+    </div>
+
+    <div style="color: red">
+      {{ message }}
+    </div>
+
+    <hr />
+
+    <!-- しりとり表示 -->
+    <div v-if="isJoined">
+      <!-- ゲームオーバー時の表示 -->
+      <div v-if="isGameOver">
+        <div style="color: red">{{ posts[0].userName }} さんの負け</div>
+        <input type="button" value="最初から" @click="restart" />
+      </div>
+
+      <!-- 入力欄 -->
+      <div v-else>
+        <div>{{ turnUserName }}さんのターン:</div>
+
+        <input type="text" v-model="input" />
+        <input type="button" value="送信" @click="postWord" />
+      </div>
+
+      <!-- 入力履歴 -->
+      <div v-for="(post, i) in posts" :key="i">
+        <div>{{ post.userName }} : " {{ post.word }} "</div>
+      </div>
+
+      <!-- 終了ボタン -->
+      <div>
+        <input type="button" value="終了" @click="exit" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import io from "socket.io-client";
+export default {
+  name: "App",
+  data: () => ({
+    userName: "",
+    joinType: 1,
+    isJoined: false,
+    roomId: "",
+    message: "",
+    input: "",
+    turnUserName: "",
+    posts: [],
+    isGameOver: false,
+    socket: io("http://localhost:3031"),
+  }),
+
+  created() {
+    this.socket.on("connect", () => {
+      console.log("connected");
+    });
+  },
+
+  mounted() {
+    this.socket.on("updateRoom", (room) => {
+      this.isJoined = true;
+      this.roomId = room.id;
+      this.message = "";
+      this.turnUserName = room.users[room.turnUserIndex].name;
+      this.posts = room.posts;
+      this.input = "";
+      this.isGameOver = room.isGameOver;
+    });
+
+    this.socket.on("notifyError", (error) => {
+      this.message = error;
+    });
+
+    this.socket.on("notifyDisconnection", (userName, turnUserName) => {
+      this.message = userName + " さんが退室しました";
+      this.turnUserName = turnUserName;
+    });
+  },
+
+  methods: {
+    createRoom() {
+      this.socket.emit("create", this.userName);
+      this.message = "";
+    },
+
+    enterRoom() {
+      this.socket.emit("enter", this.userName, this.roomId);
+      this.message = "";
+    },
+
+    postWord() {
+      this.socket.emit("post", this.input);
+      this.message = "";
+    },
+
+    restart() {
+      this.socket.emit("restart");
+      this.message = "";
+    },
+
+    exit(){
+      this.socket.emit("exit");
+      this.message = "";
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
---
+++ test_socketio/server/index.js
@@ -0,0 +1,172 @@
+const http = require("http").createServer();
+const io = require("socket.io")(http, {
+    cors: {
+        origin: ["http://localhost:8080"],
+    },
+});
+const rooms = [];
+const users = [];
+
+
+
+io.on("connection", (socket) => {
+    // 部屋を新しく建てる
+    socket.on("create", (userName) => {
+        if (userName == "") {
+            io.to(socket.id).emit("notifyError", "名前を入力してください");
+            return;
+        }
+        const roomId = generateRoomId();
+        const user = { id: socket.id, name: userName, roomId };
+        const room = {
+            id: roomId,
+            users: [user],
+            turnUserIndex: 0,
+            posts: [],
+        };
+        rooms.push(room);
+        users.push(user);
+        socket.join(roomId);
+        io.to(socket.id).emit("updateRoom", room);
+    });
+
+    // 部屋に入室する
+    socket.on("enter", (userName, roomId) => {
+        if (userName == "") {
+            io.to(socket.id).emit("notifyError", "名前を入力してください");
+            return;
+        }
+        const roomIndex = rooms.findIndex((r) => r.id == roomId);
+        if (roomIndex == -1) {
+            io.to(socket.id).emit("notifyError", "部屋が見つかりません");
+            return;
+        }
+        const user = { id: socket.id, name: userName, roomId };
+        rooms[roomIndex].users.push(user);
+        users.push(user);
+        socket.join(rooms[roomIndex].id);
+        io.to(socket.id).emit("updateRoom", rooms[roomIndex]);
+    });
+
+    // しりとりの単語を送信
+    socket.on("post", (input) => {
+        const user = users.find((u) => u.id == socket.id);
+        const roomIndex = rooms.findIndex((r) => r.id == user.roomId);
+        const room = rooms[roomIndex];
+
+        // ターンプレイヤーかチェック
+        if (room.users[room.turnUserIndex].id != socket.id) {
+            io.to(socket.id).emit("notifyError", "あなたのターンではありません");
+            return;
+        }
+        // // 正しい入力かチェック
+        // if (!checkWord(input, room.posts)) {
+        //     io.to(socket.id).emit(
+        //         "notifyError",
+        //         "入力が不正です。1つ前の単語の最後の文字から始まる単語を半角英字入力してください"
+        //     );
+        //     return;
+        // }
+        // 単語を保存
+        rooms[roomIndex].posts.unshift({
+            userName: user.name,
+            word: input,
+            // isGameOver: checkGameOver(input),
+        });
+        // ターンプレイヤーを次のユーザーに進める
+        rooms[roomIndex].turnUserIndex = getNextTurnUserIndex(room);
+
+        io.in(room.id).emit("updateRoom", room);
+    });
+
+    // 最初から始める
+    socket.on("restart", () => {
+        const user = users.find((u) => u.id == socket.id);
+        const roomIndex = rooms.findIndex((r) => r.id == user.roomId);
+        const room = rooms[roomIndex];
+        rooms[roomIndex].posts.length = 0;
+        rooms[roomIndex].isGameOver = false;
+
+        io.in(room.id).emit("updateRoom", room);
+    });
+
+    //強制終了ボタン
+    socket.on("exit", () => {
+        const user = users.find((u) => u.id == socket.id);
+        const roomIndex = rooms.findIndex((r) => r.id == user.roomId);
+        const room = rooms[roomIndex];
+        // console.log(rooms[roomIndex]);
+        rooms[roomIndex].isGameOver = true;
+        // console.log(rooms[roomIndex]);
+        io.in(room.id).emit("updateRoom", room);
+    })
+    // 接続が切れた場合
+    socket.on("disconnect", () => {
+        const user = users.find((u) => u.id == socket.id);
+        if (!user) {
+            // userデータがないときは未入室なので何もせず終了
+            return;
+        }
+        const roomIndex = rooms.findIndex((r) => r.id == user.roomId);
+        const room = rooms[roomIndex];
+        const userIndex = room.users.findIndex((u) => u.id == socket.id);
+        // ターンプレイヤーの場合、次のユーザーに進める
+        if (userIndex == room.turnUserIndex) {
+            rooms[roomIndex].turnUserIndex = getNextTurnUserIndex(room);
+        }
+        // ユーザーのデータを削除
+        rooms[roomIndex].users.splice(userIndex, 1);
+        users.splice(
+            users.findIndex((u) => u.id == socket.id),
+            1
+        );
+        // ターンプレイヤーのindexが1ズレないように補正
+        if (room.turnUserIndex > userIndex) {
+            rooms[roomIndex].turnUserIndex--;
+        }
+
+        io.in(room.id).emit(
+            "notifyDisconnection",
+            user.name,
+            room.users[rooms[roomIndex].turnUserIndex].name
+        );
+    });
+});
+
+// ランダムなroomId(1000~9999)を生成する
+function generateRoomId() {
+    const id = Math.floor(Math.random() * 8999 + 1000);
+    if (rooms.some((r) => r.id == id)) {
+        // ランダムに生成したidが既に存在する場合は作り直す
+        return generateRoomId();
+    }
+    return id;
+}
+
+// 入力が不正な値でないかチェック
+function checkWord(word, posts) {
+    // 半角英字でないならNG
+    if (!word.match(/^[a-z]+$/)) {
+        return false;
+    }
+    // 1つ目の単語の場合特にチェックなしでOK
+    if (posts.length == 0) {
+        return true;
+    }
+    // 前の単語の最後の文字から始まってるならOK
+    return word.slice(0, 1) == posts[0].word.slice(-1);
+}
+
+// 終了(xで終わる単語を入力したかどうか)判定
+function checkGameOver(word) {
+    return word.slice(-1) == "x";
+}
+
+// 次のターンプレイヤーのindexを返却
+function getNextTurnUserIndex(room) {
+    return room.turnUserIndex == room.users.length - 1
+        ? 0
+        : room.turnUserIndex + 1;
+}
+
+http.listen(3031);
