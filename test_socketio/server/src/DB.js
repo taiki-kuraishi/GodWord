35a7a402d96b9036a153214f08f8890e314c6679
taiki-kuraishi <20122027co@gmail.com>
2023-08-15 15:39:21 +0900

    DB.js

--- test_socketio/server/src/DB.js
+++ test_socketio/server/src/DB.js
@@ -14,14 +14,20 @@ client.connect(async (err) => {
     console.log('PostgreSQL Connected...');
 
     try {
-        const query = 'SELECT * FROM godwordtable'; // あなたのテーブル名に置き換える
-        const result = await client.query(query);
+        const query_1 = 'SELECT * FROM godwordtable limit 10'; // あなたのテーブル名に置き換える
+        const result_1 = await client.query(query_1);
 
         console.log('Database Contents:');
         console.log('id, title, length');
-        for (const row of result.rows) {
+        for (const row of result_1.rows) {
             console.log(`${row.id}, ${row.title}, ${row.length}`);
         }
+
+        const query_2 = 'select count(*) from godwordtable;';
+        const result_2 = await client.query(query_2); // query_2 に修正
+        console.log('DB length');
+        console.log(result_2.rows[0].count); // 結果からカウントを取得
+
     } catch (err) {
         console.error('Error querying database:', err);
     } finally {
