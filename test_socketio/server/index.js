a8ffd2c797ed115665d8f346e0d02b39228bef81
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-08-20 10:31:07 +0900

    restartに足りなかった処理の追加
    used_countの初期化
    word_score_tableの初期化
    pointsの初期化

--- test_socketio/server/index.js
+++ test_socketio/server/index.js
@@ -699,6 +699,91 @@ io.on("connection", (socket) => {
             room.cards[key] = [];
         }
 
+        //used_cardの初期化
+        rooms[roomIndex].used_count = {
+            "0": 0,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+            "4": 0,
+            "5": 0,
+            "6": 0,
+            "7": 0,
+            "8": 0,
+            "9": 0,
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0,
+            "h": 0,
+            "i": 0,
+            "j": 0,
+            "k": 0,
+            "l": 0,
+            "m": 0,
+            'n': 0,
+            "o": 0,
+            "p": 0,
+            "q": 0,
+            "r": 0,
+            "s": 0,
+            "t": 0,
+            "u": 0,
+            "v": 0,
+            "w": 0,
+            "x": 0,
+            "y": 0,
+            "z": 0,
+        };
+
+        //word_score_tableの初期化
+        rooms[roomIndex].word_score_table = {
+            '7': ["0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "i",
+                "j",
+                "k",
+                "l",
+                "m",
+                'n',
+                "o",
+                "p",
+                "q",
+                "r",
+                "s",
+                "t",
+                "u",
+                "v",
+                "w",
+                "x",
+                "y",
+                "z"], '6': [], '5': [], '4': [], '3': [], '2': [], '1': [], '-5': []
+        };
+
+        //pointの初期化
+        for (var key in room.points) {
+            room.cards[key] = 0;
+        }
+
         rooms[roomIndex].isGameOver = false;
 
         io.in(room.id).emit("updateRoom", room);
